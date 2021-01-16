4eaa6bdaf5b0dd4d107586e8175606c8fe7eb512
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-16 08:20:54 +0200

    Dependencies injection

---
+++ app/src/main/java/app/mulipati_agent/activities/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package app.mulipati_agent.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.mulipati_agent.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
