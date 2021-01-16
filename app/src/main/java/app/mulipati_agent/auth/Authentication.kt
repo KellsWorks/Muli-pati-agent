6cf134df5a5b2888e0408cf6a80a31148ae87908
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-16 09:09:50 +0200

    Auth

---
+++ app/src/main/java/app/mulipati_agent/auth/Authentication.kt
@@ -0,0 +1,12 @@
+package app.mulipati_agent.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.mulipati_agent.R
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
---
+++ app/src/main/java/app/mulipati_agent/auth/login/SignInFragment.kt
@@ -0,0 +1,21 @@
+package app.mulipati_agent.auth.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.mulipati_agent.R
+
+class SignInFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+}
