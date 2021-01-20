f20dc6db1fc7a106ed8f63a7e9f16318f9e830e7
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 09:39:43 +0200

    Subscriptions

--- app/src/main/java/app/mulipati_agent/ui/subscriptions/SubscriptionsFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/subscriptions/SubscriptionsFragment.kt
@@ -5,19 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.mulipati_agent.R
+import app.mulipati_agent.databinding.FragmentSubscribeBinding
+import app.mulipati_agent.databinding.FragmentSubscriptionsBinding
 
 
 class SubscriptionsFragment : Fragment() {
 
+    private lateinit var subscribeBinding: FragmentSubscriptionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subscriptions, container, false)
+
+        subscribeBinding = FragmentSubscriptionsBinding.inflate(inflater, container, false)
+        subscribeBinding.lifecycleOwner = this
+
+        return subscribeBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        subscribeBinding.subscriptionBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
 }
