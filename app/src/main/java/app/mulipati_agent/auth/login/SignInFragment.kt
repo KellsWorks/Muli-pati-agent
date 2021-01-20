e71ee956b4e6f9196acc20fd924775c758ed20a5
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 10:48:02 +0200

    Sign in

--- app/src/main/java/app/mulipati_agent/auth/login/SignInFragment.kt
+++ app/src/main/java/app/mulipati_agent/auth/login/SignInFragment.kt
@@ -1,11 +1,13 @@
 package app.mulipati_agent.auth.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import app.mulipati_agent.MainActivity
 import app.mulipati_agent.R
 import app.mulipati_agent.databinding.FragmentSignInBinding
 
@@ -28,7 +30,16 @@ class SignInFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         signInBinding.createAccount.setOnClickListener {
-            findNavController().navigate(R.id.action_signInFragment_to_registerFragment)
+            requireActivity()
+                .startActivity(
+                    Intent(
+                        requireActivity(), MainActivity::class.java
+                    )
+                )
+            requireActivity()
+                .overridePendingTransition(
+                    android.R.anim.slide_out_right, android.R.anim.slide_in_left
+                )
         }
     }
 }
