f1e442f061a7712bceb3961d1003d1e89c0dde99
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 11:08:47 +0200

    Update ContinuePlanFragment.kt

--- app/src/main/java/app/mulipati_agent/ui/plans/ContinuePlanFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/plans/ContinuePlanFragment.kt
@@ -1,11 +1,13 @@
 package app.mulipati_agent.ui.plans
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import app.mulipati_agent.MainActivity
 import app.mulipati_agent.databinding.FragmentContinuePlanBinding
 
 
@@ -29,6 +31,15 @@ class ContinuePlanFragment : Fragment() {
 
         continuePlanBinding.continuePlanBack.setOnClickListener {
             findNavController().navigateUp()
+        }
+
+        continuePlanBinding.continuePlan.setOnClickListener {
+            requireActivity()
+                .startActivity(
+                    Intent(
+                        requireActivity(), MainActivity::class.java
+                    )
+                )
         }
     }
 }
