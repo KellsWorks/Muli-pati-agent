917c347242d330c8ac699090ec6a98ff9cde3ca6
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 08:53:31 +0200

    Personal Fragment
    Personal Fragment

--- app/src/main/java/app/mulipati_agent/ui/personal/PersonalFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/personal/PersonalFragment.kt
@@ -1,17 +1,36 @@
 package app.mulipati_agent.ui.personal
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.mulipati_agent.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import app.mulipati_agent.databinding.FragmentPersonalBinding
 
 class PersonalFragment : Fragment() {
 
+    private lateinit var personalBinding: FragmentPersonalBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal, container, false)
+
+        personalBinding = FragmentPersonalBinding.inflate(inflater, container, false)
+        personalBinding.lifecycleOwner = this
+
+        return personalBinding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        personalBinding.personalBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun onBackPressed(){
+        findNavController().navigateUp()
     }
 }
