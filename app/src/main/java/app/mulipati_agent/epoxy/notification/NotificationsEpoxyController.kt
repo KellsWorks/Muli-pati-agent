61230ad417ea04beaa8dac5d7d528fb9a29b83ce
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 07:27:09 +0200

    Notifications

--- app/src/main/java/app/mulipati_agent/epoxy/notification/NotificationsEpoxyController.kt
+++ app/src/main/java/app/mulipati_agent/epoxy/notification/NotificationsEpoxyController.kt
@@ -3,12 +3,61 @@ package app.mulipati_agent.epoxy.notification
 import android.widget.PopupMenu
 import android.widget.Toast
 import app.mulipati.data.Notifications
+import app.mulipati_agent.R
 import com.airbnb.epoxy.Typed2EpoxyController
 import java.lang.reflect.Method
 
 
 class NotificationsEpoxyController: Typed2EpoxyController<Boolean?, List<Notifications>>() {
     override fun buildModels(status: Boolean?, notifications: List<Notifications>?) {
+        if (notifications != null) {
+            for (notification in notifications){
+                NotificationsEpoxyModel_()
+                    .id(notification.title)
+                    .data(notification)
+                    .click { _, parentView, _, _ ->
+                        val popupMenu = PopupMenu(parentView.menu!!.context, parentView.menu)
 
+                        popupMenu.menuInflater.inflate(R.menu.notifications, popupMenu.menu)
+                        popupMenu.setOnMenuItemClickListener { item ->
+                            when(item.itemId) {
+                                R.id.asRead -> {
+                                    Toast.makeText(parentView.datetime!!.context, "Marked as read!", Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+
+                                R.id.delete -> {
+                                    Toast.makeText(parentView.datetime!!.context, "Deleted!", Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+                            }
+                            true
+                        }
+
+                        try {
+                            val classPopupMenu = Class.forName(
+                                popupMenu
+                                    .javaClass.name
+                            )
+                            val mPopup =
+                                classPopupMenu.getDeclaredField("mPopup")
+                            mPopup.isAccessible = true
+                            val menuPopupHelper = mPopup[popupMenu]
+                            val classPopupHelper = Class.forName(
+                                menuPopupHelper
+                                    .javaClass.name
+                            )
+                            val setForceIcons: Method = classPopupHelper.getMethod(
+                                "setForceShowIcon", Boolean::class.javaPrimitiveType
+                            )
+                            setForceIcons.invoke(menuPopupHelper, true)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        popupMenu.show()
+                    }
+                    .addTo(this)
+            }
+        }
     }
 }
--- app/src/main/java/app/mulipati_agent/ui/profile/ProfileFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/profile/ProfileFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.mulipati_agent.R
+import app.mulipati_agent.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var profileBinding: FragmentProfileBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        profileBinding = FragmentProfileBinding.inflate(inflater, container, false)
+        profileBinding.lifecycleOwner = this
+
+        return profileBinding.root
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        profileBinding.notifictions.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_notificationsFragment)
+        }
+    }
+
 
 }
