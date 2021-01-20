f6e4789eb97ebbce692c418832656581f8e4f7f8
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 10:18:53 +0200

    Continue plan
    Continue plan

--- app/src/main/java/app/mulipati_agent/auth/subscribe/SubscribeFragment.kt
+++ app/src/main/java/app/mulipati_agent/auth/subscribe/SubscribeFragment.kt
@@ -2,10 +2,11 @@ package app.mulipati_agent.auth.subscribe
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import app.mulipati_agent.MainActivity
 import app.mulipati_agent.R
 import app.mulipati_agent.databinding.FragmentSubscribeBinding
@@ -30,9 +31,7 @@ class SubscribeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         subscribeBinding.silverSubscription.setOnClickListener {
-            requireActivity().startActivity(Intent(
-                    requireActivity(), MainActivity::class.java
-            ))
+            findNavController().navigate(R.id.action_subscribeFragment_to_continuePlanFragment)
         }
 
         subscribeBinding.bronzeSubscription.setOnClickListener {
---
+++ app/src/main/java/app/mulipati_agent/ui/plans/ContinuePlanFragment.kt
@@ -0,0 +1,22 @@
+package app.mulipati_agent.ui.plans
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.mulipati_agent.R
+
+
+class ContinuePlanFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_continue_plan, container, false)
+    }
+
+}
