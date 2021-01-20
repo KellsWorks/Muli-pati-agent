7789822396bfd10e220728e7db8a9089b66c4a6a
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-20 07:30:18 +0200

    Update NotificationsFragment.kt

--- app/src/main/java/app/mulipati_agent/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/notifications/NotificationsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.mulipati.data.Notifications
 import app.mulipati_agent.R
 import app.mulipati_agent.databinding.FragmentNotificationsBinding
@@ -24,6 +25,14 @@ class NotificationsFragment : Fragment() {
 
         return notificationsBinding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notificationsBinding.notificationsBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
