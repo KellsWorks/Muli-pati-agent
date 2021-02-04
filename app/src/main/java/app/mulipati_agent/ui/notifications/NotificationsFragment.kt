53eec24500751db74b924dfde4cf539ad506e42f
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-04 08:56:05 +0200

    NotificationsFragment

--- app/src/main/java/app/mulipati_agent/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/notifications/NotificationsFragment.kt
@@ -1,21 +1,35 @@
 package app.mulipati_agent.ui.notifications
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import app.mulipati.data.Notifications
+import app.mulipati.util.Resource
 import app.mulipati_agent.R
 import app.mulipati_agent.databinding.FragmentNotificationsBinding
 import app.mulipati_agent.epoxy.notification.NotificationsEpoxyController
+import app.mulipati_agent.network.responses.notifications.Notification
+import app.mulipati_agent.util.autoCleared
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
-
+@AndroidEntryPoint
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsBinding: FragmentNotificationsBinding
+    private var notificationsBinding: FragmentNotificationsBinding by  autoCleared()
+
     private lateinit var controller: NotificationsEpoxyController
+
+    private val notificationsViewModel: NotificationsViewModel by viewModels()
+
+    private lateinit var notificationsList: ArrayList<Notifications>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -39,16 +53,6 @@ class NotificationsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 
-        val notifications = ArrayList<Notifications>()
-
-        notifications.add(Notifications(R.drawable.ic_bell_ring, "Booking successful!", "You have booked a trip on 2 February 2021 at 4pm", "4 Feb, 2021 11:45AM"))
-        notifications.add(Notifications(R.drawable.ic_bell_ring, "Booking successful!", "You have booked a trip on 2 February 2021 at 4pm", "4 Feb, 2021 11:45AM"))
-
-        controller = NotificationsEpoxyController()
-        controller.setData(true, notifications)
-
-        notificationsBinding.recentNotificationsRecycler
-            .setController(controller)
 
         val old = ArrayList<Notifications>()
 
@@ -60,6 +64,57 @@ class NotificationsFragment : Fragment() {
 
         notificationsBinding.viewedNotificationsRecycler
             .setController(controllerOne)
+    }
+
+    private fun setupObservers() {
+        val getId = context?.getSharedPreferences("user", Context.MODE_PRIVATE)?.getInt("id", 0)
+        notificationsViewModel.notications.observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Resource.Status.SUCCESS -> {
+                    try {
+                        if (it.data?.get(0)?.user_id ==  2){
+                            notificationsList = ArrayList()
+                            for (notify in it.data){
+                                if (notify.id == getId && notify.status == "unmarked"
+                                ) {
+
+                                    notificationsList.add(
+                                        Notifications(
+                                            R.drawable.ic_bell_ring, notify.title, notify.content, notify.created_at
+                                        )
+                                    )
+
+                                }
+                                else{
+                                    Timber.e("No notifications")
+                                }
+                            }
+
+                            setUpRecycler(notificationsList)
+                        }
+                    }catch (e: IndexOutOfBoundsException){
+                        Timber.e(e)
+                    }
+                }
+
+                Resource.Status.ERROR ->
+                    Toast.makeText(activity, it.message, Toast.LENGTH_SHORT).show()
+
+                Resource.Status.LOADING -> {
+
+                }
+            }
+        })
+    }
+
+    private fun setUpRecycler(data: List<Notifications>) {
+
+        controller = NotificationsEpoxyController()
+        controller.setData(true, data)
+
+        notificationsBinding.recentNotificationsRecycler
+            .setController(controller)
+
     }
 
 }
--- app/src/main/java/app/mulipati_agent/util/Constants.kt
+++ app/src/main/java/app/mulipati_agent/util/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val PROFILE_URL = "http://192.168.43.200:322/storage/profile/"
 
     const val IMAGE_REQUEST = 1
+
+    const val DB_NAME = "Muli_pati_agent"
 }
