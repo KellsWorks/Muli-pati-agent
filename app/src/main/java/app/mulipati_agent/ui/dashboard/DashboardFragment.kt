94657e2c29c9ecb3881c73e491c0f2b71eb942fd
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-06 10:29:05 +0200

    BackPressed Dispatcher

--- app/src/main/java/app/mulipati_agent/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/app/mulipati_agent/ui/dashboard/DashboardFragment.kt
@@ -2,6 +2,7 @@ package app.mulipati_agent.ui.dashboard
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -83,7 +85,19 @@ class DashboardFragment : Fragment() {
 
         bindLocation()
 
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
+            val exitIntent = Intent(
+                    Intent.ACTION_MAIN
+            )
+            exitIntent.addCategory(Intent.CATEGORY_HOME)
+            exitIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            requireActivity().startActivity(
+                    exitIntent
+            )
+        }
+
     }
+
 
     private fun bindLocation() {
 
